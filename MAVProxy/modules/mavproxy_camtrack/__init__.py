89466962146a8c3733f93d3a99077bb865283e69
Rhys Mainwaring <rhys.mainwaring@me.com>
2024-09-27 21:37:33 +0100

    camtrack: initial structure for camera tracking module
    - Add initial structure for module
    - Use MPImage to render RTSP stream
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ MAVProxy/modules/mavproxy_camtrack/__init__.py
@@ -0,0 +1,92 @@
+"""
+MAVProxy camera tracking module
+"""
+
+from MAVProxy.modules.lib import mp_module
+from MAVProxy.modules.lib import mp_util
+
+from pymavlink import mavutil
+
+from MAVProxy.modules.mavproxy_camtrack.camera_view import CameraView
+
+
+class CamTrackModule(mp_module.MPModule):
+    """A tool to control camera tracking"""
+
+    def __init__(self, mpstate):
+        super(CamTrackModule, self).__init__(
+            mpstate, "camtrack", "camera tracking module"
+        )
+
+        # GUI
+        rtsp_url = "rtsp://127.0.0.1:8554/camera"
+        self.camera_view = CameraView(title="Camera", rtsp_url=rtsp_url)
+
+        # mavlink messages
+
+        # data
+
+        # control update rate to GUI
+        self._msg_list = []
+        self._fps = 30.0
+        self._last_send = 0.0
+        self._send_delay = (1.0 / self._fps) * 0.9
+
+        # commands
+        self.add_command("camtrack", self.cmd_camtrack, "camera tracking")
+
+    def cmd_camtrack(self, args):
+        """Control behaviour of commands"""
+        if len(args) == 0:
+            print(self.usage())
+        elif args[0] == "status":
+            print(self.status())
+        else:
+            print(self.usage())
+
+    def usage(self):
+        """Show help on command line options."""
+        return "Usage: camtrack <status>"
+
+    def status(self):
+        """Return information about the camera tracking state"""
+        return {}
+
+    def mavlink_packet(self, m):
+        """Handle a mavlink packet."""
+        pass
+
+    def check_events(self):
+        """Check for events on the camera view"""
+        self.camera_view.check_events()
+
+        # tell mavproxy to unload the module if the GUI is closed
+        # if self.camera_view.close_event.wait(timeout=0.001):
+        #     self.needs_unloading = True
+
+    def send_messages(self):
+        """Send message list via pipe to GUI at desired update rate"""
+        if (time.time() - self._last_send) > self._send_delay:
+            # pipe data to GUI
+            self.camera_view.parent_pipe_send.send(self._msg_list)
+
+            # reset counters etc
+            self._msg_list = []
+            self._last_send = time.time()
+
+    def idle_task(self):
+        """Idle tasks"""
+        self.check_events()
+        self.send_messages()
+
+    def unload(self):
+        """Close the GUI and unload module"""
+
+        # close the GUI
+        self.camera_view.close()
+
+
+def init(mpstate):
+    """Initialise module"""
+
+    return CamTrackModule(mpstate)
---
+++ MAVProxy/modules/mavproxy_camtrack/camera_view.py
@@ -0,0 +1,123 @@
+"""
+MAVProxy camera view
+"""
+
+import sys
+import time
+
+from MAVProxy.modules.lib.mp_image import MPImage
+from MAVProxy.modules.lib.mp_menu import MPMenuItem
+from MAVProxy.modules.lib.mp_image import MPImageTrackPos
+from MAVProxy.modules.lib.mp_image import MPImageFrameCounter
+
+
+class CameraView:
+    """Handle a camera view"""
+
+    def __init__(self, title, rtsp_url, fps=30):
+        self.frame_width = 640
+        self.frame_height = 480
+
+        self.frame_counter = -1
+        self.rtsp_url = rtsp_url
+        self.tracking = False
+
+        self.im = MPImage(
+            # title=title,
+            mouse_events=True,
+            mouse_movement_events=False,
+            # width=self.frame_width,
+            # height=self.frame_height,
+            key_events=True,
+            can_drag=False,
+            can_zoom=False,
+            auto_size=False,
+            auto_fit=True,
+            # fps=fps,
+        )
+
+        # Capture video
+        gst_pipeline = (
+            f"rtspsrc location={self.rtsp_url} latency=50 "
+            "! decodebin "
+            "! videoconvert "
+            "! video/x-raw,format=(string)BGR "
+            "! videoconvert "
+            "! appsink emit-signals=true sync=false max-buffers=2 drop=true"
+        )
+        self.im.set_gstreamer(gst_pipeline)
+
+    def close(self):
+        """Close the GUI"""
+        # TODO: MPImage does not have a close_event
+        # trigger a close event which is monitored by the
+        # child gui process - it will close allowing the
+        # process to be joined
+        # self.im.close_event.set()
+        # if self.im.is_alive():
+        #     self.im.child.join(timeout=2.0)
+        self.im.terminate()
+
+    def is_alive(self):
+        """Check if the GUI process is alive"""
+        return self.im.is_alive()
+
+    def start_tracking(self):
+        """Start object tracking"""
+        self.tracking = True
+        if self.im is not None:
+            self.im.start_tracking()
+
+    def end_tracking(self):
+        """End object tracking"""
+        self.tracking = False
+        if self.im is not None:
+            self.im.end_tracking()
+
+    def check_events(self):
+        """Check for events"""
+        if self.im is None:
+            return
+        if not self.is_alive():
+            return
+        for event in self.im.events():
+            if isinstance(event, MPImageTrackPos):
+                continue
+            if isinstance(event, MPImageFrameCounter):
+                self.frame_counter = event.frame
+                continue            
+            if (
+                hasattr(event, "ClassName")
+                and event.ClassName == "wxMouseEvent"
+                and event.leftIsDown
+            ):
+                track_size_pct = 10.0
+                if event.shiftDown:
+                    (xres, yres) = (event.shape[1], event.shape[0])
+                    twidth = int(yres * 0.01 * track_size_pct)
+                    self.end_tracking()
+            
+                    self.im.start_tracker(event.X, event.Y, twidth, twidth)
+                    self.tracking = True
+                elif event.controlDown:
+                    self.end_tracking()
+                else:
+                    pass
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+
+    parser = OptionParser("camera_view.py [options]")
+    parser.add_option("--rtsp-server", default=None, type=str, help="RTSP URL")
+
+    (opts, args) = parser.parse_args()
+    if opts.rtsp_server is None:
+        print("Must specify an RTSP URL")
+        sys.exit(1)
+
+    camera_view = CameraView("Camera View", opts.rtsp_server)
+
+    while True:
+        time.sleep(0.1)
+        camera_view.check_events()
