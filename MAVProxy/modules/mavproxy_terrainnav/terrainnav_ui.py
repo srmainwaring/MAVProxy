d5a712ca599ed1afe15010e5022b460b8a53f138
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-02-24 13:24:51 +0000

    terrainnav: add module for terrain navigation
    - Add module folder.
    - Add module and app backend.
    - Add wx ui frame.
    - Add buttons and send commands to backend.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ MAVProxy/modules/mavproxy_terrainnav/terrainnav_ui.py
@@ -0,0 +1,171 @@
+"""
+Terrain navigation UI
+"""
+
+import time
+
+from MAVProxy.modules.lib.wx_loader import wx
+
+from MAVProxy.modules.mavproxy_terrainnav import terrainnav_msgs
+
+
+class TerrainNavFrame(wx.Frame):
+    def __init__(self, state, title, size):
+        super().__init__(None, title=title, size=size)
+        self._state = state
+
+        # control update rate
+        self._timer = wx.Timer(self)
+        self._fps = 10.0
+        self._start_time = time.time()
+
+        # buttons
+        button_size = (100, 25)
+        self._button_set_start = wx.Button(
+            self, id=wx.ID_ANY, label="Set Start", size=button_size
+        )
+        self._button_set_goal = wx.Button(
+            self, id=wx.ID_ANY, label="Set Goal", size=button_size
+        )
+        self._button_add_rally = wx.Button(
+            self, id=wx.ID_ANY, label="Add Rally", size=button_size
+        )
+        self._button_add_wp = wx.Button(
+            self, id=wx.ID_ANY, label="Add Waypoint", size=button_size
+        )
+        self._button_run_planner = wx.Button(
+            self, id=wx.ID_ANY, label="Run Planner", size=button_size
+        )
+        self._button_hold = wx.Button(
+            self, id=wx.ID_ANY, label="Hold", size=button_size
+        )
+        self._button_navigate = wx.Button(
+            self, id=wx.ID_ANY, label="Navigate", size=button_size
+        )
+        self._button_rollout = wx.Button(
+            self, id=wx.ID_ANY, label="Rollout", size=button_size
+        )
+        self._button_abort = wx.Button(
+            self, id=wx.ID_ANY, label="Abort", size=button_size
+        )
+        self._button_return = wx.Button(
+            self, id=wx.ID_ANY, label="Return", size=button_size
+        )
+
+        # layout
+        self._vert_sizer1 = wx.BoxSizer(wx.VERTICAL)
+        self._vert_sizer1.Add(self._button_set_start, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer1.Add(self._button_set_goal, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer1.Add(self._button_add_rally, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer1.Add(self._button_add_wp, proportion=0, flag=wx.EXPAND)
+
+        self._vert_sizer2 = wx.BoxSizer(wx.VERTICAL)
+        self._vert_sizer2.Add(self._button_run_planner, proportion=0, flag=wx.EXPAND)
+
+        self._vert_sizer3 = wx.BoxSizer(wx.VERTICAL)
+        self._vert_sizer3.Add(self._button_hold, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer3.Add(self._button_navigate, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer3.Add(self._button_rollout, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer3.Add(self._button_abort, proportion=0, flag=wx.EXPAND)
+        self._vert_sizer3.Add(self._button_return, proportion=0, flag=wx.EXPAND)
+
+        self._horz_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._horz_sizer.Add(self._vert_sizer1, proportion=0, flag=wx.EXPAND)
+        self._horz_sizer.Add(self._vert_sizer2, proportion=0, flag=wx.EXPAND)
+        self._horz_sizer.Add(self._vert_sizer3, proportion=0, flag=wx.EXPAND)
+        self.SetSizer(self._horz_sizer)
+
+        # button events
+        self.Bind(wx.EVT_BUTTON, self.on_set_start_pushed, self._button_set_start)
+        self.Bind(wx.EVT_BUTTON, self.on_set_goal_pushed, self._button_set_goal)
+        self.Bind(wx.EVT_BUTTON, self.on_add_rally_pushed, self._button_add_rally)
+        self.Bind(wx.EVT_BUTTON, self.on_add_wp_pushed, self._button_add_wp)
+
+        self.Bind(wx.EVT_BUTTON, self.on_run_planner_pushed, self._button_run_planner)
+
+        self.Bind(wx.EVT_BUTTON, self.on_hold_pushed, self._button_hold)
+        self.Bind(wx.EVT_BUTTON, self.on_navigate_pushed, self._button_navigate)
+        self.Bind(wx.EVT_BUTTON, self.on_rollout_pushed, self._button_rollout)
+        self.Bind(wx.EVT_BUTTON, self.on_abort_pushed, self._button_abort)
+        self.Bind(wx.EVT_BUTTON, self.on_return_pushed, self._button_return)
+
+        # events
+        self.Bind(wx.EVT_TIMER, self.on_timer, self._timer)
+        self.Bind(wx.EVT_IDLE, self.on_idle)
+        self.Bind(wx.EVT_CHAR_HOOK, self.on_key_press)
+
+        # restart the timer
+        self._timer.Start(milliseconds=100)
+
+    def on_timer(self, event):
+        """
+        Handle timer events.
+        """
+        # check for close events
+        if self._state.close_event.wait(timeout=0.001):
+            # stop the timer and destroy the window - this ensures
+            # the window is closed whhen the module is unloaded
+            self._timer.Stop()
+            self.Destroy()
+            return
+
+        # receive data from the module
+        while self._state.ui_pipe_recv.poll():
+            obj_list = self._state.ui_pipe_recv.recv()
+            for obj in obj_list:
+                pass
+
+        # reset counters
+        self._start_time = time.time()
+
+    def on_idle(self, event):
+        """
+        Handle idle events.
+        """
+        pass
+
+    def on_key_press(self, event):
+        """
+        Handle keypress events.
+        """
+        pass
+
+    def on_set_start_pushed(self, event):
+        msg = terrainnav_msgs.SetStart()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_set_goal_pushed(self, event):
+        msg = terrainnav_msgs.SetGoal()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_add_rally_pushed(self, event):
+        msg = terrainnav_msgs.AddRally()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_add_wp_pushed(self, event):
+        msg = terrainnav_msgs.AddWaypoint()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_run_planner_pushed(self, event):
+        msg = terrainnav_msgs.RunPlanner()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_hold_pushed(self, event):
+        msg = terrainnav_msgs.Hold()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_navigate_pushed(self, event):
+        msg = terrainnav_msgs.Navigate()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_rollout_pushed(self, event):
+        msg = terrainnav_msgs.Rollout()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_abort_pushed(self, event):
+        msg = terrainnav_msgs.Abort()
+        self._state.ui_pipe_send.send(msg)
+
+    def on_return_pushed(self, event):
+        msg = terrainnav_msgs.Return()
+        self._state.ui_pipe_send.send(msg)
