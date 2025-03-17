5984c77566ddefd5cefcf1353c3193369d8031b9
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-03-17 23:37:03 +0000

    terrainnav: formatting and remove commented code
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- MAVProxy/modules/mavproxy_terrainnav/terrainnav_msgs.py
+++ MAVProxy/modules/mavproxy_terrainnav/terrainnav_msgs.py
@@ -2,54 +2,67 @@
 Terrain Navigation messages
 """
 
+
 class SetStart:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class SetGoal:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class AddRally:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class AddWaypoint:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class RunPlanner:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class Hold:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class Navigate:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class Rollout:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class Abort:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class Return:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class ShowContours:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class HideContours:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
+
 
 class RemoveContours:
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
